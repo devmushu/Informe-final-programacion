89ef4489f1a78bb231d3d1f94d68f3cb026506f4
iscojavier <franciscoxyz7@gmail.com>
2024-05-24 12:28:42 -0400

    modificacion hasheo codigo

--- Evaluacion Final/ejercicio_prueba.py
+++ Evaluacion Final/ejercicio_prueba.py
@@ -1,44 +1,45 @@
 
-#==========================================sanitizar textos==============================================================================#
-import re #importamos libreria para sanitizar entradas
+#?==========================================sanitizar textos==============================================================================#
+import re #todo importamos libreria para sanitizar entradas
 
 def sanitizador(x): #funcion para sanitizar texto
     sano = re.sub(r'[^a-zA-Z0-9\s]', '', x)
-    sano = sano.capitalize().strip() # la capitalizamos y eliminas espacios vacios
+    sano = sano.capitalize().strip() #todo la capitalizamos y eliminas espacios vacios
     return sano
-#==========================================================================================================================================#
-#==========================hasheo password========================================================================================================#
+#?==========================================================================================================================================#
+#?==========================hasheo password========================================================================================================#
 import hashlib
 import os
 
 def hasheo(contraseña, salt=None):
     if salt is None:
-        salt = os.urandom(16)  # Genera un salt aleatoria de 16 bytes
+        salt = os.urandom(16)  #todo Genera un salt aleatoria de 16 bytes
     else:
         salt = salt.encode('utf-8')
     # Combina la contraseña y la sal antes de hashear
-    contraseñaHash = hashlib.sha256(salt + password.encode('utf-8')).hexdigest()
+    contraseñaHash = hashlib.sha256(salt + contraseña.encode('utf-8')).hexdigest()
     return contraseñaHash, salt.hex()
 
 def verificacion_Pass(password, stored_password_hash, salt):
     # Recalcula el hash de la contraseña con la misma sal
     new_password_hash = hashlib.sha256(bytes.fromhex(salt) + password.encode('utf-8')).hexdigest()
-    # Compara el hash calculado con el hash almacenado
+
     return new_password_hash == stored_password_hash
 
+#?=======================================================anonimizacion=========================================================================================================#
+def anonimacion_data(data):
+    # Sustituir cada carácter por 'X' para anonimizar los datos
+    datoAnonimo = '*' * (len(data))
+    return datoAnonimo
 
+#?==============================^trabajo hecho para informe^=========================================================================================================#
 
+clientes = {} #*====> lista vacia
+usuarios = {}  #*====> lista vacia x2
+idcliente = 0 #*=====> contador id cliente
+idusuario = 0 #*=====> Contador idusuario
 
-
-
-#==============================^trabajo hecho para informe^=========================================================================================================#
-
-clientes = {} #====> lista vacia
-usuarios = {}  #====> lista vacia x2
-idcliente = 0 #=====> contador id cliente
-idusuario = 0 #=====> Contador idusuario
-
-def menuprincipal(): #===========> FUNCION QUE IMPRIME EL MENU PRINCIPAL
+def menuprincipal(): #?===========> FUNCION QUE IMPRIME EL MENU PRINCIPAL
     print("================================")
     print("   M E N Ú  P R I N C I P A L   ")
     print("================================")
@@ -49,7 +50,7 @@ def menuprincipal(): #===========> FUNCION QUE IMPRIME EL MENU PRINCIPAL
     print("       5.- (E) Salir            ")
     print("================================")
 
-def menumostrar():  # ==============> FUNCION QUE IMPRIME EL MENU MOSTRAR
+def menumostrar():  #?==============> FUNCION QUE IMPRIME EL MENU MOSTRAR
     print("================================")
     print("     M E N Ú  M O S T R A R     ")
     print("================================")
@@ -59,17 +60,18 @@ def menumostrar():  # ==============> FUNCION QUE IMPRIME EL MENU MOSTRAR
     print("       4.- VOLVER               ")
     print("================================")
 
-def ingresardatos(): #=========> FUNCION QUE SOLICITA LOS DATOS DEL CLIENTE
+def ingresardatos(): #?=========> FUNCION QUE SOLICITA LOS DATOS DEL CLIENTE
     print("=================================")
     print("     INGRESAR DATOS CLIENTE      ")
     print("=================================")
-    run = sanitizador(input("INGRESE RUN : "))  #=============> sanitizar entrada de texto de manera simple °
-    nombre=sanitizador(input("INGRESE NOMBRE : ")) #=============> sanitizar entrada de texto   °
-    apellido=sanitizador(input("INGRESE APELLIDO : "))  #=============> sanitizar entrada de texto  ° 
-    direccion=sanitizador(input("INGRESE DIRECCION : ")) #=============> sanitizar entrada de texto     °
-    fono=sanitizador(input("INGRESE TELEFONO : ")) #=============> sanitizar entrada de texto   °
-    correo=sanitizador(input("INGRESE CORREO : ")) #=============> sanitizar entrada de texto   °
-#     // necesitamos validar si los campos no son vacios //
+    run = sanitizador(input("INGRESE RUN : "))  #todo=============> sanitizar entrada de texto de manera simple °
+    nombre=sanitizador(input("INGRESE NOMBRE : ")) #todo=============> sanitizar entrada de texto   °
+    apellido=sanitizador(input("INGRESE APELLIDO : "))  #todo=============> sanitizar entrada de texto  ° 
+    direccion=sanitizador(input("INGRESE DIRECCION : ")) #todo=============> sanitizar entrada de texto     °
+    fono=sanitizador(input("INGRESE TELEFONO : ")) #todo=============> sanitizar entrada de texto   °
+    correo=sanitizador(input("INGRESE CORREO : ")) #todo=============> sanitizar entrada de texto   °
+    #!necesitamos validar si los campos no son vacios //
+
     print(nombre,apellido)
     tipos = [
         #k    #v
@@ -89,23 +91,23 @@ def ingresardatos(): #=========> FUNCION QUE SOLICITA LOS DATOS DEL CLIENTE
     cliente = [codigo,run,nombre,apellido,direccion,fono,correo,tipo,monto,deuda]
     clientes[idcliente]=cliente
 
-def mostrar():    #===========> FUNCION PARA LLAMAR TODOS LOS MENUS
+def mostrar():    #?===========> FUNCION PARA LLAMAR TODOS LOS MENUS
     while(True):
         menumostrar()
         op2 = int(input("  INGRESE OPCIÓN : "))
-        if op2 == 1: #OPCION 1
+        if op2 == 1: #?OPCION 1
             mostrartodo()
             input("\n\n PRESIONE ENTER PARA CONTINUAR")
-        elif op2 == 2: #OPCION 2
+        elif op2 == 2: #?OPCION 2
             mostraruno()
-        elif op2 == 3: #OPCION 3
+        elif op2 == 3: #?OPCION 3
             mostrarparcial()
-        if op2 == 4: #OPCION 4
+        if op2 == 4: #?OPCION 4
             break
-        else:   #OPCION FUERA DEL RANGO
+        else:   #?OPCION FUERA DEL RANGO
             print("Opción Fuera de Rango")
 
-def mostrartodo():  #===========> FUNCION PARA MOSTRAR LOS CLIENTES
+def mostrartodo():  #?===========> FUNCION PARA MOSTRAR LOS CLIENTES
     print("=================================")
     print("  MUESTRA DE TODOS LOS CLIENTES  ")
     print("=================================")
@@ -115,7 +117,7 @@ def mostrartodo():  #===========> FUNCION PARA MOSTRAR LOS CLIENTES
                 cliente, dato[1], dato[2], dato[3], dato[4], dato[5], dato[6] , dato[8], dato[9], dato[7]))
         print("-------------------------------------------------------------------------------------------------------------------------------------------------")
 
-def mostraruno(): #==================> FUNCION PARA MOSTRAR UN DATO DE UN USUARIO
+def mostraruno(): #?==================> FUNCION PARA MOSTRAR UN DATO DE UN USUARIO
     print("=================================")
     print("   MUESTRA DE DATOS PARTICULAR   ")
     print("=================================")
@@ -138,7 +140,7 @@ def mostraruno(): #==================> FUNCION PARA MOSTRAR UN DATO DE UN USUARI
     print("-----------------------------------------")
     input("\n\n PRESIONE ENTER PARA CONTINUAR")
 
-def mostrarparcial(): #==================> FUUNCION PARA MOSTRAR CIERTA CANTIDAD DE DATOS
+def mostrarparcial(): #?==================> FUUNCION PARA MOSTRAR CIERTA CANTIDAD DE DATOS
     print("=======================================")
     print("   MUESTRA PARCIALMENTE LOS CLIENTES   ")
     print("=======================================")
@@ -152,7 +154,7 @@ def mostrarparcial(): #==================> FUUNCION PARA MOSTRAR CIERTA CANTIDAD
         print("-------------------------------------------------------------------------------------------------------------------------------------------------")
     input("\n\n PRESIONE ENTER PARA CONTINUAR")
 
-def modificardatos(): #==================>FUNCION PARA MODIFICAR AL CLIENTE SELECCIONADO
+def modificardatos(): #?==================>FUNCION PARA MODIFICAR AL CLIENTE SELECCIONADO
     listanuevos=[]
     print("===================================")
     print("      MODULO MODIFICAR CLIENTE     ")
@@ -227,7 +229,7 @@ def modificardatos(): #==================>FUNCION PARA MODIFICAR AL CLIENTE SELE
     clientes[mod]=listanuevos
 
 
-def eliminardatos():  #===============> FUNCION PARA ELIMINAR UN CLIENTE
+def eliminardatos():  #?===============> FUNCION PARA ELIMINAR UN CLIENTE
     print("===================================")
     print("      MODULO ELIMINAR CLIENTE      ")
     print("===================================")
@@ -235,9 +237,9 @@ def eliminardatos():  #===============> FUNCION PARA ELIMINAR UN CLIENTE
     elim = int(input("Ingrese valor de ID del Cliente que desea Eliminar : "))
     del clientes[elim]
 
-# --------------------------------------
+#! --------------------------------------
 
-def menuUsuarios():    #==================> FUNCION PARA MOSTRAR MENU INICIO DE SESION
+def menuUsuarios():    #?==================> FUNCION PARA MOSTRAR MENU INICIO DE SESION
     print("================================")
     print("   M E N Ú  U S U A R I O S     ")
     print("================================")
@@ -246,71 +248,72 @@ def menuUsuarios():    #==================> FUNCION PARA MOSTRAR MENU INICIO DE 
     print("       3.-  Salir               ")
     print("================================")
 
-def ingresoUsuarios():    #==================> FUNCION PARA REGISTRAR EN MENU INICIAR SESION
+def ingresoUsuarios():    #?==================> FUNCION PARA REGISTRAR EN MENU INICIAR SESION
     print("=======================================")
     print("        INGRESO DE USUARIO             ")
     print("=======================================")
     username = input( "INGRESE NOMBRE DE USUARIO:  ")
     clave = input( "INGRESE PASSWORD         : ")
-    #======================================================
+    #todo======================================================
     claveHash, salt = hasheo(clave)
-    #==============================================================
+    #todo==============================================================
     nombre = input(   "INGRESE NOMBRE           : ")
     apellidos = input("INGRESE APELLIDOS        : ")
     correo = input(   "INGRESE CORREO           : ")
     print("=======================================")
-    global idusuario  #=============> La variable la convierte en globlal para que lo tome el contador de afuera
+    global idusuario  #?=============> La variable la convierte en globlal para que lo tome el contador de afuera
     idusuario += 1
     codigo = idusuario
-    usuario = [codigo,username,clave,claveHash,salt,nombre,apellidos,correo]
+    usuario = [codigo,username,claveHash,salt,nombre,apellidos,correo]
     usuarios[username] = usuario
 
 
-while True: #===========> creamos el while del menu y lo mostramos llamando funciones
+while True: #?===========> creamos el while del menu y lo mostramos llamando funciones
     menuUsuarios()
     try:
-        opUsu = int(input("INGRESE OPCIÓN: "))  #=========> Seleccion de opcion
+        opUsu = int(input("INGRESE OPCIÓN: "))  #todo=========> Seleccion de opcion
 
-        if opUsu == 1:  #============> opcion 1 iniciar sesion
+        if opUsu == 1:  #?============> opcion 1 iniciar sesion 
             user = input("Ingrese nombre de usuario: ")
-            clave = input("Ingrese password: ")         #//hay que hacshear encriptar la clave
+            clave = input("Ingrese password: ")        #todo hay que hacshear encriptar la clave
             if usuarios.get(user):
                 usuario = usuarios.get(user)
-                if usuario[2] == clave:
-                    print(f"Bienvenido {usuario[3]} {usuario[4]} - {usuario[2]} - id: {usuario[0]}.")
+                if verificacion_Pass(clave, usuario[2],usuario[3]): #todo==============>inicio de sesion con verificacion de Contraseña encriptada 
+                    print(f"Bienvenido {usuario[4]} {usuario[5]} - {anonimacion_data(clave)} - id: {usuario[0]}.") #todo=====> anonimizo datos aunque quitaria la linea de codigo
                     input("Presiona ENTRAR para ingresar al Menú Principal.")
-                    while True:  # Bucle para el Menú Principal
-                        menuprincipal()
-                        try: #==========================================> agregamos try para evitar la caida del programa
-                            op = int(input("INGRESE OPCIÓN: "))
-                            if op == 1:
-                                ingresardatos()
-                            elif op == 2:
-                                mostrar()
-                            elif op == 3:
-                                modificardatos()
-                            elif op == 4:
-                                eliminardatos()
-                            elif op == 5:
-                                opSalir = input("¿DESEA SALIR [SI/NO]: ")
-                                if opSalir.lower() == "si":
-                                    break  # Salir del bucle del Menú Principal
-                        except:
-                            print('ERROR')
-                        else:
-                            print("Opción Fuera de Rango")
-                    break  # Salir del bucle del Menú de Usuarios
+                    while True:  #? Bucle para el Menú Principal
+                            menuprincipal()
+                            try: #todo==========================================> agregamos try para evitar la caida del programa
+                                op = int(input("INGRESE OPCIÓN: "))
+                                if op == 1:
+                                    ingresardatos()
+                                elif op == 2:
+                                    mostrar()
+                                elif op == 3:
+                                    modificardatos()
+                                elif op == 4:
+                                    eliminardatos()
+                                elif op == 5:
+                                    opSalir = input("¿DESEA SALIR [SI/NO]: ")
+                                    if opSalir.lower() == "si":
+                                        break  #? Salir del bucle del Menú Principal
+                            except:
+                                print('ERROR')
+                            else:
+                                print("Opción Fuera de Rango")
+                    break  #? Salir del bucle del Menú de Usuarios
                 else:
-                    input("Contraseña incorrecta. Presiona ENTER para volver al Menú de Usuarios.")
+                        input("Contraseña incorrecta. Presiona ENTER para volver al Menú de Usuarios.")
             else:
-                input("Usuario no registrado. Presiona ENTER para volver al Menú de Usuarios.")
-        elif opUsu == 2: #=================> opcion crear usuario
+                    input("Usuario no registrado. Presiona ENTER para volver al Menú de Usuarios.")
+        elif opUsu == 2: #?=================> opcion crear usuario
             ingresoUsuarios()
-        elif opUsu == 3: #================> opcion salir
+        elif opUsu == 3: #?================> opcion salir
             opSalir = input("¿DESEA SALIR [SI/NO]: ")
             if opSalir.lower() == "si":
                 break
+        else: #?====================> seleccion fuera de rango
+             print("Opción Fuera de Rango")
     except:
         print('Valor no valido')
-    else: #====================> seleccion fuera de rango
-        print("Opción Fuera de Rango")
+
